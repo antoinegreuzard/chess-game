50ec1cefb1d8d6b629f7f05a9507b0ee2852d350
antoinegreuzard <antoinegreuzard@users.noreply.github.com>
2024-10-26 19:14:22 +0000

    deploy: 34674d5540636aeff826576402c148c450a311e8

--- dist/vite.config.js
+++ dist/vite.config.js
@@ -1,8 +1,29 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 export default defineConfig({
-    root: 'public', // Spécifie le dossier racine pour servir les fichiers
-    base: '/chess-game/',
+    root: 'public',
+    base: '/chess-game/', // Base URL pour GitHub Pages
     build: {
         outDir: '../dist',
+        rollupOptions: {
+            output: {
+                entryFileNames: '[name].js',
+                chunkFileNames: '[name].js',
+                assetFileNames: '[name].[ext]',
+                preserveModules: true,
+                format: 'esm',
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        return 'vendor';
+                    }
+                },
+            },
+        },
+    },
+    resolve: {
+        alias: {
+            '@': resolve(__dirname, 'src'),
+        },
+        extensions: ['.ts', '.js'],
     },
 });
