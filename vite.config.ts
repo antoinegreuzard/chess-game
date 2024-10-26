7a5da96943e9ea4e473f54b4e32ba6f615bd07bd
antoinegreuzard <antoine@antoinegreuzard.fr>
2024-10-26 21:24:26 +0200

    fix: ça marche pas le build

--- vite.config.ts
+++ vite.config.ts
@@ -3,28 +3,26 @@ import { resolve } from 'path';
 
 export default defineConfig({
   root: 'public',
-  base: '/chess-game/', // Base URL pour GitHub Pages
+  base: '/chess-game/',
   build: {
     outDir: '../dist',
+    minify: 'terser', // Utilise esbuild pour minimiser le code
+    sourcemap: true,   // Génère une carte source pour le debugging
     rollupOptions: {
       output: {
-        entryFileNames: '[name].js',
-        chunkFileNames: '[name].js',
-        assetFileNames: '[name].[ext]',
+        format: 'es', // Utilise le format ES Modules
         preserveModules: true,
-        format: 'esm',
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
-        },
+        preserveModulesRoot: 'src', // Préserve la structure des modules
+        entryFileNames: '[name].js', // Conserve .js dans les fichiers générés
+        chunkFileNames: '[name]-[hash].js',
+        assetFileNames: '[name].[ext]',
       },
     },
   },
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src'),
+      '@': resolve(__dirname, 'src'), // Alias pour src
     },
-    extensions: ['.ts', '.js'],
+    extensions: ['.ts', '.js'], // Résoudre les extensions .ts et .js
   },
 });
