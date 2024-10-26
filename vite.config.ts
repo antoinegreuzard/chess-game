a8c1d05b7e476e5c54813cbb0f084a810b883a2c
Antoine Greuzard <antoin.greuzard@gmail.com>
2024-10-26 21:41:40 +0200

    Develop (#10)
    * feat: add title, new sidebar with piece killed and pass round system
    
    * feat: add reload on new game
    
    * feat: update ts
    
    * feat: add checkmate
    
    * feat: add some rule for checkmate and king
    
    * feat: add actions
    
    * fix: actions bug and tsc bug
    
    * feat: update vite test
    
    * fix: delete step server
    
    * fix: tsc error
    
    * feat: use module
    
    * fix: run prettier
    
    * fix: change publish_dir
    
    * fix
    
    * Create SECURITY.md (#2)
    
    Signed-off-by: Antoine Greuzard <antoin.greuzard@gmail.com>
    
    * fix: github pages not working
    
    * fix
    
    * fix: use verbatimModuleSyntax
    
    * fix: github page error with js
    
    * fix: ça marche pas le build
    
    * fix: prettier
    
    * fix: system reset
    
    * fix: only in src
    
    ---------
    
    Signed-off-by: Antoine Greuzard <antoin.greuzard@gmail.com>
    Co-authored-by: antoinegreuzard <antoine@antoinegreuzard.fr>

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,27 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
-  root: 'public', // Spécifie le dossier racine pour servir les fichiers
+  root: '',
   base: '/chess-game/',
   build: {
-    outDir: '../dist',
+    outDir: 'public',
+    minify: 'esbuild', // Utilise esbuild pour minimiser le code
+    sourcemap: true, // Génère une carte source pour le debugging
+    rollupOptions: {
+      output: {
+        format: 'es', // Utilise le format ES Modules
+        preserveModulesRoot: 'src', // Préserve la structure des modules
+        entryFileNames: '[name].js', // Conserve .js dans les fichiers générés
+        chunkFileNames: '[name]-[hash].js',
+        assetFileNames: '[name].[ext]',
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'), // Alias pour src
+    },
+    extensions: ['.ts', '.js'], // Résoudre les extensions .ts et .js
   },
 });
