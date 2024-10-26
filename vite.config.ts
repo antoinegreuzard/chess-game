874a886f4021780a45126a85925f2cf517d7350b
antoinegreuzard <antoine@antoinegreuzard.fr>
2024-10-26 21:45:06 +0200

    fix: remove public

--- vite.config.ts
+++ vite.config.ts
@@ -2,21 +2,11 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  root: '',
   base: '/chess-game/',
   build: {
     outDir: 'public',
     minify: 'esbuild', // Utilise esbuild pour minimiser le code
     sourcemap: true, // Génère une carte source pour le debugging
-    rollupOptions: {
-      output: {
-        format: 'es', // Utilise le format ES Modules
-        preserveModulesRoot: 'src', // Préserve la structure des modules
-        entryFileNames: '[name].js', // Conserve .js dans les fichiers générés
-        chunkFileNames: '[name]-[hash].js',
-        assetFileNames: '[name].[ext]',
-      },
-    },
   },
   resolve: {
     alias: {
