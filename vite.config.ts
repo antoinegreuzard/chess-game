21c0124ca41e329d607e329c4956334e9af51ad1
antoinegreuzard <antoine@antoinegreuzard.fr>
2024-10-26 20:46:10 +0200

    fix: github pages not working

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: 'public', // Spécifie le dossier racine pour servir les fichiers
+  base: '/chess-game/',
   build: {
     outDir: '../dist',
   },
