6f49d44963302c4b3e3fe90fe256be29d91ce6a1
antoinegreuzard <antoine@antoinegreuzard.fr>
2024-10-27 14:43:18 +0100

    feat: update utils test

--- tests/utils.test.ts
+++ tests/utils.test.ts
@@ -1,5 +1,5 @@
-// tests/utils.test.ts
-import { showMessage } from '../src/utils';
+import { showMessage, updateCapturedPieces } from '../src/utils';
+import { PieceColor, PieceType } from '../src/piece';
 
 describe('showMessage', () => {
   let gameMessageElement: HTMLElement;
@@ -26,5 +26,37 @@ describe('showMessage', () => {
     showMessage('');
     expect(gameMessageElement.textContent).toBe('');
     expect(gameMessageElement.style.display).toBe('block');
+  });
+});
+
+describe('updateCapturedPieces', () => {
+  let capturedWhiteElement: HTMLElement;
+  let capturedBlackElement: HTMLElement;
+
+  beforeAll(() => {
+    // Ajoute des éléments DOM pour les pièces capturées
+    capturedWhiteElement = document.createElement('div');
+    capturedWhiteElement.id = 'capturedWhite';
+    document.body.appendChild(capturedWhiteElement);
+
+    capturedBlackElement = document.createElement('div');
+    capturedBlackElement.id = 'capturedBlack';
+    document.body.appendChild(capturedBlackElement);
+  });
+
+  afterAll(() => {
+    // Nettoie le DOM après les tests
+    document.body.removeChild(capturedWhiteElement);
+    document.body.removeChild(capturedBlackElement);
+  });
+
+  test('should update captured white pieces', () => {
+    updateCapturedPieces(PieceType.PAWN, PieceColor.BLACK);
+    expect(capturedBlackElement.textContent).toBe('♟');
+  });
+
+  test('should update captured black pieces', () => {
+    updateCapturedPieces(PieceType.ROOK, PieceColor.WHITE);
+    expect(capturedWhiteElement.textContent).toBe('♖');
   });
 });
