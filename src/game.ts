24e44328e9be7beda978f9c4d2414e10666217c1
antoinegreuzard <antoine@antoinegreuzard.fr>
2024-10-28 08:53:00 +0100

    refactor

--- src/game.ts
+++ src/game.ts
@@ -13,7 +13,7 @@ export class Game {
   }
 
   public start(): void {
-    console.log('Nouvelle partie d\'échecs démarrée !');
+    console.log("Nouvelle partie d'échecs démarrée !");
   }
 
   public getBoard(): Board {
@@ -37,7 +37,7 @@ export class Game {
       if (move) {
         this.board.movePiece(move.fromX, move.fromY, move.toX, move.toY);
       } else {
-        console.log('Aucun mouvement valide pour l\'IA.');
+        console.log("Aucun mouvement valide pour l'IA.");
       }
     }
   }
