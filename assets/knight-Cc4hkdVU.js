0b142c4603c831548f9ed20a1c648bcc1eb17159
antoinegreuzard <antoinegreuzard@users.noreply.github.com>
2024-10-30 18:30:02 +0000

    deploy: 374388e41d371e521a913a87a63eab761c525b01

---
+++ assets/knight-Cc4hkdVU.js
@@ -0,0 +1,2 @@
+import{P as n,a as o}from"./ai.worker-CBr2oSXN.js";class h extends n{constructor(s){super(s,o.KNIGHT)}isValidMove(s,c,e,a,i){const t=Math.abs(e-s),r=Math.abs(a-c);return(t===2&&r===1||t===1&&r===2)&&this.canCapture(e,a,i)}}export{h as Knight};
+//# sourceMappingURL=knight-Cc4hkdVU.js.map
