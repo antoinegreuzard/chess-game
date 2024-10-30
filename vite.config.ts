5e0c3a79c0264a7cf02673d1f15e10c3fc42e703
Antoine GREUZARD <antoine.greuzard@cgi.com>
2024-10-30 09:18:21 +0100

    Revert "fix vite build"
    This reverts commit 6e598e2d02ac6a656628a2eac64a82e572836e3d.

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,12 @@ export default defineConfig({
   base: '/chess-game/',
   build: {
     outDir: 'dist',
-    minify: false,
+    minify: 'esbuild',
     target: 'esnext',
     sourcemap: true,
     rollupOptions: {
       output: {
-        inlineDynamicImports: true, // Force l’inclusion de tous les imports dynamiques dans un seul fichier
-        entryFileNames: 'bundle.js', // Spécifie le nom du fichier final
-        chunkFileNames: undefined, // Empêche la création de chunks additionnels
-        assetFileNames: '[name].[ext]',
+        format: 'es',
       },
     },
   },
