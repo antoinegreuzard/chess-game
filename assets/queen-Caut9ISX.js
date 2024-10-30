70889117d19287929200837912e28d12f4c31e10
antoinegreuzard <antoinegreuzard@users.noreply.github.com>
2024-10-30 09:05:13 +0000

    deploy: 34d584db4394faa5aafceee0656021d95cc90226

--- assets/queen-Caut9ISX.js
+++
@@ -1,2 +0,0 @@
-import{P as t,a as c}from"./ai.worker-CZpNziBD.js";class h extends t{constructor(e){super(e,c.QUEEN)}isValidMove(e,i,s,a,r){return(e===s||i===a||Math.abs(s-e)===Math.abs(a-i))&&this.isPathClear(e,i,s,a,r)?this.canCapture(s,a,r):!1}}export{h as Queen};
-//# sourceMappingURL=queen-Caut9ISX.js.map
