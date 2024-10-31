177fa6ca797014046a3d8c64910736c9123e60b8
antoinegreuzard <antoinegreuzard@users.noreply.github.com>
2024-10-31 16:20:05 +0000

    deploy: e02259e80d645b85263771f10122a9ab80347baf

---
+++ assets/bishop-BllwhRoF.js
@@ -0,0 +1,2 @@
+import{P as t,a as c}from"./index-Cu-YNL3q.js";class n extends t{constructor(e){super(e,c.BISHOP)}isValidMove(e,i,s,a,r){return Math.abs(s-e)===Math.abs(a-i)&&this.isPathClear(e,i,s,a,r)?this.canCapture(s,a,r):!1}}export{n as Bishop};
+//# sourceMappingURL=bishop-BllwhRoF.js.map
