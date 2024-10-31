0023ba862bf662cf02eab225c4e0c29e6834b0fa
antoinegreuzard <antoinegreuzard@users.noreply.github.com>
2024-10-31 19:08:01 +0000

    deploy: dd3c2bb86d80d95ad62f43aa09cfb091e2fbdcf0

---
+++ assets/knight-Cf_l6Qya.js
@@ -0,0 +1,2 @@
+import{P as t,a as p}from"./ai.worker-CXU6DqCI.js";class h extends t{constructor(a){super(a,p.KNIGHT)}isValidMove(a,i,e,s,n){if(e<0||e>=8||s<0||s>=8)return!1;const r=Math.abs(e-a),c=Math.abs(s-i);return(r===2&&c===1||r===1&&c===2)&&this.canCapture(e,s,n)}}export{h as Knight};
+//# sourceMappingURL=knight-Cf_l6Qya.js.map
