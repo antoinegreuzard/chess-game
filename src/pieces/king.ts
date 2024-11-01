cdcc8dfbac5eb930196dcbef481ac1dac77904eb
Antoine Greuzard <antoin.greuzard@gmail.com>
2024-11-01 17:19:51 +0100

    Develop (#91)
    * Feature/save game (#65)
    
    * feat: add saving game
    
    * refactor
    
    * fix
    
    * fix: some bug
    
    * feat: better ai
    
    * fix: tests en passant
    
    * fix: isValidMove returns true for en passant capture
    
    * fix: isValidMove returns true for en passant capture
    
    * fix: add true
    
    * Create CODE_OF_CONDUCT.md
    
    Signed-off-by: Antoine Greuzard <antoin.greuzard@gmail.com>
    
    * Update issue templates
    
    * refactor ai
    
    * refactor board
    
    * refactor piece
    
    * refactor pieces
    
    * refactor utils
    
    * refactor canvas
    
    * refactor timer
    
    * fix: drag impossible on ai pieces
    
    * fix: remove save game
    
    ---------
    
    Signed-off-by: Antoine Greuzard <antoin.greuzard@gmail.com>
    Co-authored-by: Antoine GREUZARD <antoine.greuzard@cgi.com>
    
    * fix: remove saveGameToFile
    
    * Update pull_request_template.md
    
    Signed-off-by: Antoine Greuzard <antoin.greuzard@gmail.com>
    
    * fix: remove saveGameToFile
    
    * refactor: add ai folder
    
    * Create dependabot.yml
    
    Signed-off-by: Antoine Greuzard <antoin.greuzard@gmail.com>
    
    * feat: add some jobs
    
    * fix: add token
    
    * fix: rename token to github_token
    
    * feat: add letters and x,y on canvas
    
    * remove dependabot
    
    * feat: add @size-limit
    
    * remove size-limit
    
    * remove size-limit
    
    * remove size-limit job
    
    * feat: better sort
    
    * feat: better quiescence
    
    * feat: better board evaluation
    
    * feat: better opening move
    
    * feat: add tests
    
    * feat: add tests
    
    * feat: add tests
    
    * feat: add tests
    
    * feat: add tests
    
    * feat: add tests
    
    * refactor
    
    * refactor
    
    * refactor: console.log deleted
    
    * refactor
    
    * restore handlePromotion
    
    * fix: add static
    
    ---------
    
    Signed-off-by: Antoine Greuzard <antoin.greuzard@gmail.com>
    Co-authored-by: Antoine GREUZARD <antoine.greuzard@cgi.com>

--- src/pieces/king.ts
+++ src/pieces/king.ts
@@ -15,7 +15,6 @@ export class King extends Piece {
     toY: number,
     board: BoardInterface,
   ): boolean {
-    // Vérifie que la destination est dans les limites du plateau
     if (toX < 0 || toX >= 8 || toY < 0 || toY >= 8) {
       return false;
     }
@@ -25,7 +24,6 @@ export class King extends Piece {
 
     // Mouvement classique du roi
     if (dx <= 1 && dy <= 1) {
-      // Vérifie que la case cible contient une pièce ennemie ou est vide
       return (
         this.canCapture(toX, toY, board) &&
         !board.isAdjacentToAnotherKing(toX, toY, this.color)
@@ -34,28 +32,23 @@ export class King extends Piece {
 
     // Vérifications pour le roque
     if (!this.hasMoved && dy === 0 && dx === 2) {
-      // Vérifie que la position de départ du roi n'est pas sous attaque
       if (board.isSquareUnderAttack(fromX, fromY, this.color)) {
         return false;
       }
 
-      const direction = toX > fromX ? 1 : -1; // Vers la droite ou la gauche
-      const rookX = toX > fromX ? 7 : 0; // Position initiale de la tour
+      const direction = toX > fromX ? 1 : -1;
+      const rookX = toX > fromX ? 7 : 0;
       const rook = board.getPiece(rookX, fromY);
 
-      // Assure que la tour est présente, n'a pas bougé, et est de type Rook
       if (rook && rook.type === PieceType.ROOK && !rook.hasMoved) {
-        // Vérifie que chaque case entre le roi et la tour est libre et non attaquée
         for (let x = fromX + direction; x !== toX; x += direction) {
           if (
-            board.getPiece(x, fromY) || // Vérifie si la case est occupée
-            board.isSquareUnderAttack(x, fromY, this.color) // Vérifie si la case est sous attaque
+            board.getPiece(x, fromY) ||
+            board.isSquareUnderAttack(x, fromY, this.color)
           ) {
             return false;
           }
         }
-
-        // Vérifie que la case finale du roi n'est pas sous attaque et que le roi n'est pas adjacent à un autre roi
         return (
           !board.isSquareUnderAttack(toX, fromY, this.color) &&
           !board.isAdjacentToAnotherKing(toX, toY, this.color)
@@ -64,5 +57,17 @@ export class King extends Piece {
     }
 
     return false;
+  }
+
+  // Nouvelle méthode pour vérifier les menaces sans règles spécifiques du roi
+  public static isThreatenedMove(
+    fromX: number,
+    fromY: number,
+    toX: number,
+    toY: number,
+  ): boolean {
+    const dx = Math.abs(toX - fromX);
+    const dy = Math.abs(toY - fromY);
+    return dx <= 1 && dy <= 1;
   }
 }
