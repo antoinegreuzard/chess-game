1e86fb11296be8977c4ff914bf6f8cdcdebeedd4
antoinegreuzard <antoinegreuzard@users.noreply.github.com>
2024-11-01 14:35:41 +0000

    deploy: c42ad59de7f8a05748778d571db7c2cdb69e0a63

---
+++ assets/rook-CjKau_2b.js
@@ -0,0 +1,2 @@
+import{P as c,a as t,c as o}from"./index-BSOrO4mD.js";class v extends c{type=t.ROOK;hasMoved=!1;constructor(e){super(e,t.ROOK)}isValidMove(e,a,s,r,i){return s<0||s>=8||r<0||r>=8||!(e===s||a===r)||!this.isPathClear(e,a,s,r,i)?!1:this.canCapture(s,r,i)}toData(){return{...super.toData(),hasMoved:this.hasMoved}}static async fromData(e){const a=await o(t.ROOK,e.color);return a.hasMoved=e.hasMoved,a}}export{v as Rook};
+//# sourceMappingURL=rook-CjKau_2b.js.map
