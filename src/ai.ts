f1f9396de1552617f9a1e2ec24f772f114bc7953
antoinegreuzard <antoin.greuzard@gmail.com>
2024-11-02 20:53:44 +0100

    feat: diffrent maxtime at each turn

--- src/ai.ts
+++ src/ai.ts
@@ -18,7 +18,7 @@ export class AI {
     string,
     { value: number; depth: number }
   >; // Table de transposition avec profondeur
-  private readonly maxTime: number; // Temps maximum de réflexion en millisecondes
+  private maxTime: number; // Temps maximum de réflexion en millisecondes
   private startTime: number;
   private readonly killerMoves: Map<
     number,
@@ -40,7 +40,7 @@ export class AI {
 
   constructor(
     private readonly color: PieceColor,
-    maxTime: number = Math.floor(Math.random() * (50000 - 5000 + 1)) + 5000,
+    maxTime: number = 5000,
   ) {
     this.transpositionTable = new Map();
     this.maxTime = maxTime;
@@ -70,6 +70,7 @@ export class AI {
     }
 
     this.startTime = Date.now();
+    this.maxTime = Math.floor(Math.random() * (50000 - 5000 + 1)) + 5000;
 
     const openingMove = this.getOpeningMove(board);
     if (
