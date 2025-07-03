92ee5fb394ca8ee8efb2de84bb1bc60fa79f0c17
antoinegreuzard <antoinegreuzard@users.noreply.github.com>
2025-07-03 10:01:24 +0000

    deploy: d39ca69372bf532253f717be83e84e8b91950f1d

--- pr-266/assets/queen-B4KArwK2.js
+++
@@ -1,2 +0,0 @@
-import{P as n,a as u}from"./index-BxkcdxSC.js";class l extends n{constructor(a){super(a,u.QUEEN)}isValidMove(a,i,e,s,r){return e<0||e>=8||s<0||s>=8?!1:(a===e||i===s||Math.abs(e-a)===Math.abs(s-i))&&this.isPathClear(a,i,e,s,r)?this.canCapture(e,s,r):!1}}export{l as Queen};
-//# sourceMappingURL=queen-B4KArwK2.js.map
