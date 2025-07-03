a7b03d5f05ab94aaa72d78cd1cbfae6071d83a57
antoinegreuzard <antoinegreuzard@users.noreply.github.com>
2025-07-03 12:23:37 +0000

    deploy: eb9d9ddf2b62f960bc0e9612bbea39a5d26e6293

--- pr-266/assets/knight-BX4fgOOq.js
+++
@@ -1,2 +0,0 @@
-import{P as t,a as p}from"./ai.worker-BWJlaGph.js";class h extends t{constructor(a){super(a,p.KNIGHT)}isValidMove(a,i,e,s,n){if(e<0||e>=8||s<0||s>=8)return!1;const r=Math.abs(e-a),c=Math.abs(s-i);return(r===2&&c===1||r===1&&c===2)&&this.canCapture(e,s,n)}}export{h as Knight};
-//# sourceMappingURL=knight-BX4fgOOq.js.map
